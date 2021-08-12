99dbcec77ef1773a7c62f35a6aa74911c0807885
unknown <leoneljara3@gmail.com>
2021-08-12 14:51:59 -0400

    finished hero title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,14 @@ import * as React from "react";
 import title from "../images/hero_title.png";
 const Hero = () => {
   return (
-    <div className="hero__title mx-auto">
-      <img src={title} alt="Title called Leonel Jara" />
+    <div className="block hero">
+      <div className="text-center my-16">
+        <div className="hero__title">
+          <h1>Leonel</h1>
+          <h1 className="hero__second">Jara</h1>
+        </div>
+        <h4 className="hero__adword"></h4>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        title: "14rem",
+      },
+      screens: {
+        xs: "320px",
+      },
+    },
   },
   variants: {
     extend: {},
