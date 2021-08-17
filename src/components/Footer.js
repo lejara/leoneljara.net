1def851e7748d71cd699bb5a1ec33fe7c58d6ca8
unknown <leoneljara3@gmail.com>
2021-08-17 13:56:31 -0400

    finished footer, added a tag stlying

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,15 @@ import * as React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div></div>
-      This is footer
-    </footer>
+    <div>
+      <hr className="footer__line" />
+      <footer class="py-4 px-3">
+        <p>&copy;{new Date().getFullYear()} Leonel Jara</p>
+        <p>
+          <a href="https://github.com/lejara/leoneljara.me">Source Code</a>
+        </p>
+      </footer>
+    </div>
   );
 };
 
