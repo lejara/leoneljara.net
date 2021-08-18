ed5d604923d1c7cb8fc2963f966bfe5dd0bb97be
unknown <leoneljara3@gmail.com>
2021-08-18 17:05:03 -0400

    Projects done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
 import * as React from "react";
 
-const Card = ({ title, children, image_src, image_alt }) => {
+const Card = ({ title, children, image_src, image_alt, link }) => {
   return (
     <div className={`card`}>
       <img className="card__image" src={image_src} alt={image_alt} />
 
-      <div className="card__content p-4">
-        <h3>{title}</h3>
-        <div className="card__desc">{children}</div>
+      <div className="card__content p-5">
+        <div className="mt-14">
+          <h3 className="card__title">{title}</h3>
+          <p className="card__desc mt-5">{children}</p>
+          <div className="card__action--link mt-2">
+            <a className="link-primary" href={link}>
+              Have a Look →
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,53 @@
 import * as React from "react";
 import dtc_icon from "../images/DTC_ICON.png";
+import sol_icon from "../images/SOL_ICON.png";
 
 import Card from "./Card";
 
 const Projects = () => {
   return (
-    <div className="block-component">
+    <div className="block-component projects">
       <h2 className="text-title">Previous Works</h2>
       <h3 className="text-subtitle">
         filler text, blah blahs, yes another filler text, here it is hahah wowo
         so good
       </h3>
-      <div className="works__grid pt-16">
+      <div className="works__grid py-14">
         <div className="works__item--1">
           <Card
             title="Da Twitch Charades"
             image_src={dtc_icon}
-            image_alt="Picture of Test"
+            image_alt="Logo of Twitch Charades"
+            link="https://datwitchcharades.page/"
           >
-            Content in card 1
+            Web App that allowed influencers on the platform Twtich.tv to play
+            charades with viewers in the chat. Was recived with great sucess by
+            thousands of visiters and spectators.
+            <div className="mt-4">
+              Open-source project; developed as mobile first mind-set. Made
+              using JS, SASS, HTML, and Bootstrap Overrides
+            </div>
           </Card>
         </div>
         <div className="works__item--2">
-          <Card title="Sol" image_src={dtc_icon} image_alt="Picture of Test" />
+          <Card
+            title="Solitary"
+            image_src={sol_icon}
+            image_alt="Logo of Solitary"
+            link="https://github.com/PopeSpaceous/Solitary"
+          >
+            Devloped and designed the backend of the 2D Puzzle Platformer
+            Solitary. Players must slove a series of puzzles through out a level
+            in order to escape the decaying ship.
+            <div className="mt-4">
+              Part of the orignal 4 person team; Now an Open-source project!
+              Made using Unity with C#
+            </div>
+          </Card>
         </div>
       </div>
       {/* TODO: add more projects link*/}
+      <div className="h-20"></div>
     </div>
   );
 };
