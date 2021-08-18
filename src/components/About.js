a9a0615fda3e43561300e8de9088deb78c50c3a6
unknown <leoneljara3@gmail.com>
2021-08-17 21:23:25 -0400

    started svg icons

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-
+import { StaticImage } from "gatsby-plugin-image";
+import GitHubIcon from "../images/svg/github.svg";
 const About = () => {
   return (
     <div className="block-component">
@@ -11,7 +12,11 @@ const About = () => {
           that? Yes, yes he can. The end, now you know everything!
         </div>
         <div className="about__image p-2">img</div>
-        <div className="about__socials p-2">socials</div>
+        <div className="about__socials p-2">
+          <a href="https://twitter.com/Leption_LJ"></a>
+          <a href="https://github.com/lejara"></a>
+          <GitHubIcon className="about__socials--github" />
+        </div>
       </div>
     </div>
   );
