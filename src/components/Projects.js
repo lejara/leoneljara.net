d4186fb930606b9fb6acf7aeb61d32dfc8e9d794
unknown <leoneljara3@gmail.com>
2021-08-19 15:46:58 -0400

    remove neddless div

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,49 +7,47 @@ import Card from "./Card";
 const Projects = () => {
   return (
     <div className="block-component projects">
-      <div className="">
-        <h2 className="text-title">Previous Works</h2>
-        <h3 className="text-subtitle">
-          filler text, blah blahs, yes another filler text, here it is hahah
-          wowo so good
-        </h3>
-        <div className="works__grid py-10">
-          <div className="works__item--1">
-            <Card
-              title="Da Twitch Charades"
-              image_src={dtc_icon}
-              image_alt="Logo of Twitch Charades"
-              link="https://datwitchcharades.page/"
-            >
-              Web App that allowed influencers on the platform Twtich.tv to play
-              charades with viewers in the chat. Was recived with great sucess
-              by thousands of visiters and spectators.
-              <div className="mt-4">
-                Open-source project; developed as mobile first mind-set. Made
-                using JS, SASS, HTML, and Bootstrap Overrides
-              </div>
-            </Card>
-          </div>
-          <div className="works__item--2">
-            <Card
-              title="Solitary"
-              image_src={sol_icon}
-              image_alt="Logo of Solitary"
-              link="https://github.com/PopeSpaceous/Solitary"
-            >
-              Devloped and designed the backend of the 2D Puzzle Platformer
-              Solitary. Players must slove a series of puzzles through out a
-              level in order to escape the decaying ship.
-              <div className="mt-4">
-                Part of the orignal 4 person team; Now an Open-source project!
-                Made using Unity with C#
-              </div>
-            </Card>
-          </div>
+      <h2 className="text-title">Previous Works</h2>
+      <h3 className="text-subtitle">
+        filler text, blah blahs, yes another filler text, here it is hahah wowo
+        so good
+      </h3>
+      <div className="works__grid py-10">
+        <div className="works__item--1">
+          <Card
+            title="Da Twitch Charades"
+            image_src={dtc_icon}
+            image_alt="Logo of Twitch Charades"
+            link="https://datwitchcharades.page/"
+          >
+            Web App that allowed influencers on the platform Twtich.tv to play
+            charades with viewers in the chat. Was recived with great sucess by
+            thousands of visiters and spectators.
+            <div className="mt-4">
+              Open-source project; developed as mobile first mind-set. Made
+              using JS, SASS, HTML, and Bootstrap Overrides
+            </div>
+          </Card>
         </div>
-        {/* TODO: add more projects link*/}
-        <div className="h-20"></div>
+        <div className="works__item--2">
+          <Card
+            title="Solitary"
+            image_src={sol_icon}
+            image_alt="Logo of Solitary"
+            link="https://github.com/PopeSpaceous/Solitary"
+          >
+            Devloped and designed the backend of the 2D Puzzle Platformer
+            Solitary. Players must slove a series of puzzles through out a level
+            in order to escape the decaying ship.
+            <div className="mt-4">
+              Part of the orignal 4 person team; Now an Open-source project!
+              Made using Unity with C#
+            </div>
+          </Card>
+        </div>
       </div>
+      {/* TODO: add more projects link*/}
+      <div className="h-20"></div>
     </div>
   );
 };
