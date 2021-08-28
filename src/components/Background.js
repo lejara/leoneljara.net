4fdcbaad8ccd92167dfb65502eb6ce791e92990c
unknown <leoneljara3@gmail.com>
2021-08-27 22:29:34 -0400

    anothe background changes, hope this is better X8

--- src/components/Background.js
+++ src/components/Background.js
@@ -50,7 +50,7 @@ const Background = ({ bg_containerRef }) => {
           },
           detectRetina: true,
           duration: 0,
-          fpsLimit: 30,
+          fpsLimit: 45,
           motion: {
             disable: false,
             reduce: {
@@ -62,7 +62,7 @@ const Background = ({ bg_containerRef }) => {
             bounce: {
               horizontal: {
                 random: {
-                  enable: true,
+                  enable: false,
                   minimumValue: 0.1,
                 },
                 value: 1,
@@ -73,6 +73,30 @@ const Background = ({ bg_containerRef }) => {
                   minimumValue: 0.1,
                 },
                 value: 1,
+              },
+            },
+            collisions: {
+              bounce: {
+                horizontal: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0.1,
+                  },
+                  value: 1,
+                },
+                vertical: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0.1,
+                  },
+                  value: 1,
+                },
+              },
+              enable: true,
+              mode: "bounce",
+              overlap: {
+                enable: true,
+                retries: 0,
               },
             },
             color: {
@@ -101,14 +125,98 @@ const Background = ({ bg_containerRef }) => {
                 },
               },
             },
+            destroy: {
+              mode: "none",
+              split: {
+                count: 1,
+                factor: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0,
+                  },
+                  value: 3,
+                },
+                rate: {
+                  random: {
+                    enable: false,
+                    minimumValue: 0,
+                  },
+                  value: {
+                    min: 4,
+                    max: 9,
+                  },
+                },
+                sizeOffset: true,
+              },
+            },
+            groups: {},
+            life: {
+              count: 0,
+              delay: {
+                random: {
+                  enable: false,
+                  minimumValue: 0,
+                },
+                value: 0,
+                sync: false,
+              },
+              duration: {
+                random: {
+                  enable: false,
+                  minimumValue: 0.0001,
+                },
+                value: 0,
+                sync: false,
+              },
+            },
+            links: {
+              blink: false,
+              color: {
+                value: "random",
+              },
+              consent: false,
+              distance: 100,
+              enable: false,
+              frequency: 1,
+              opacity: 1,
+              shadow: {
+                blur: false,
+                color: {
+                  value: "#021bc8",
+                },
+                enable: false,
+              },
+              triangles: {
+                enable: false,
+                frequency: 1,
+              },
+              width: 1,
+              warp: false,
+            },
             move: {
               angle: {
                 offset: 1,
                 value: 97,
               },
+              attract: {
+                distance: 200,
+                enable: false,
+                rotate: {
+                  x: 3000,
+                  y: 3000,
+                },
+              },
+              decay: 0,
               distance: {},
               direction: "bottom",
+              drift: 0,
               enable: true,
+              gravity: {
+                acceleration: 9.81,
+                enable: false,
+                inverse: false,
+                maxSpeed: 50,
+              },
               outModes: {
                 default: "out",
                 bottom: "out",
@@ -116,20 +224,41 @@ const Background = ({ bg_containerRef }) => {
                 right: "out",
                 top: "out",
               },
+              random: false,
+              size: false,
               speed: 0.4,
+              spin: {
+                acceleration: 0,
+                enable: false,
+              },
+              straight: false,
+              trail: {
+                enable: false,
+                length: 4,
+                fillColor: {
+                  value: "#0a0d26",
+                },
+              },
+              vibrate: false,
+              warp: false,
             },
             number: {
+              density: {
+                enable: false,
+                area: 900,
+                factor: 1000,
+              },
               limit: 0,
               value: 100,
             },
             opacity: {
               random: {
                 enable: true,
-                minimumValue: 0.8,
+                minimumValue: 0.7,
               },
               value: {
-                min: 0.8,
-                max: 1,
+                min: 0.7,
+                max: 0.9,
               },
               animation: {
                 count: 0,
@@ -137,7 +266,7 @@ const Background = ({ bg_containerRef }) => {
                 speed: 0.53,
                 sync: false,
                 destroy: "none",
-                minimumValue: 0.6,
+                minimumValue: 0.3,
                 startValue: "random",
               },
             },
@@ -148,21 +277,31 @@ const Background = ({ bg_containerRef }) => {
               },
               enable: true,
               offset: {
-                x: 1,
-                y: 1,
+                x: 0,
+                y: 0,
               },
             },
             shape: {
+              options: {},
               type: "circle",
             },
             size: {
               random: {
                 enable: true,
-                minimumValue: 1,
+                minimumValue: 2,
               },
               value: {
-                min: 1.7,
-                max: 2.2,
+                min: 2,
+                max: 2,
+              },
+              animation: {
+                count: 0,
+                enable: true,
+                speed: 2,
+                sync: false,
+                destroy: "none",
+                minimumValue: 1,
+                startValue: "max",
               },
             },
             zIndex: {
@@ -177,11 +316,12 @@ const Background = ({ bg_containerRef }) => {
             },
           },
           pauseOnBlur: false,
-          pauseOnOutsideViewport: false,
+          pauseOnOutsideViewport: true,
           responsive: [
             {
               maxWidth: 600,
               options: {
+                fpsLimit: 30,
                 particles: {
                   number: {
                     limit: 0,
