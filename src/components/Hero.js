70fedb249ad405024b5a16e4b4ad460b3768dba2
lejara <leoneljara3@gmail.com>
2022-06-22 15:28:15 -0400

    Remove frags

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,27 +5,25 @@ import Game from "../components/Game";
 const Hero = ({ bg }) => {
   const { playing, won } = React.useContext(gameContext);
   return (
-    <>
-      <div className="py-16 hero mb-2">
-        <div
-          className={`hero__content mx-auto ${
-            playing ? "opacity-0 invisible" : "opacity-100 visible"
-          }`}
-        >
-          <div className="hero__title mx-5">
-            <h1>Leonel</h1>
-            <h1 className="hero__second">Jara</h1>
-            <h2 className="text-white hero__adword text-left">
-              {won
-                ? "Knows Your The Best!"
-                : "Web Dev/Game Dev/Anything In Between"}
-            </h2>
-          </div>
+    <div className="py-16 hero mb-2">
+      <div
+        className={`hero__content mx-auto ${
+          playing ? "opacity-0 invisible" : "opacity-100 visible"
+        }`}
+      >
+        <div className="hero__title mx-5">
+          <h1>Leonel</h1>
+          <h1 className="hero__second">Jara</h1>
+          <h2 className="text-white hero__adword text-left">
+            {won
+              ? "Knows Your The Best!"
+              : "Web Dev/Game Dev/Anything In Between"}
+          </h2>
         </div>
-
-        <Game bg={bg} />
       </div>
-    </>
+
+      <Game bg={bg} />
+    </div>
   );
 };
 
