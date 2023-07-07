3f50bda5f415e7bca5ffe77bf35dfb9f93a95ca9
lejara <leoneljara3@gmail.com>
2023-07-07 15:24:38 -0400

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-const Footer = ({ innerStyle }) => {
+const Footer = () => {
   return (
-    <footer className={` ${innerStyle} py-4 px-3`}>
+    <footer className={` text-lg flex w-full justify-between py-4 px-3`}>
       <p>&copy;{new Date().getFullYear()} Leonel Jara</p>
       <p>
         <a href="https://github.com/lejara/leoneljara.net">Source Code</a>
